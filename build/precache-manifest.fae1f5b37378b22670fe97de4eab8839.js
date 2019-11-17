8a19bca43d4dbd6c2ca2c42fbfb5819053874b0e
shaokuangkuang <314462224@qq.com>
2019-11-17 20:49:00 +0800

    first

---
+++ build/precache-manifest.fae1f5b37378b22670fe97de4eab8839.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e54c65fb1caa6693f7f0e39f799f99e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5b97dfe1560397bd000e",
+    "url": "./static/css/2.6f60e7f3.chunk.css"
+  },
+  {
+    "revision": "128162a8df65d3be5ed9",
+    "url": "./static/css/main.0bc1bbd2.chunk.css"
+  },
+  {
+    "revision": "5b97dfe1560397bd000e",
+    "url": "./static/js/2.e6e54d60.chunk.js"
+  },
+  {
+    "revision": "128162a8df65d3be5ed9",
+    "url": "./static/js/main.58e81721.chunk.js"
+  },
+  {
+    "revision": "56a6b2399647f3d207bd",
+    "url": "./static/js/runtime-main.f1c4a8af.js"
+  },
+  {
+    "revision": "26b84fedbf5e6813812e74eb030c5f81",
+    "url": "./static/media/住吧首页_02.26b84fed.jpg"
+  },
+  {
+    "revision": "74c3c9d522b0401ccdfa91668c86e88d",
+    "url": "./static/media/住吧首页_06.74c3c9d5.jpg"
+  },
+  {
+    "revision": "4b6d56fac7999d324947c9d59d8f1882",
+    "url": "./static/media/住吧首页_13.4b6d56fa.jpg"
+  },
+  {
+    "revision": "cbbd4f3bede7cbbc491b01bcfa6d8449",
+    "url": "./static/media/商城_03.cbbd4f3b.jpg"
+  },
+  {
+    "revision": "b6407f1b1788bd294000f408fb6b2f3c",
+    "url": "./static/media/我的.b6407f1b.png"
+  },
+  {
+    "revision": "2be6f037dbd483308e7015363dc76239",
+    "url": "./static/media/灵感_03.2be6f037.jpg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cc3195d8be5f9be4b7d5e2d70c6fb2d4.js"
+  "./precache-manifest.fae1f5b37378b22670fe97de4eab8839.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
